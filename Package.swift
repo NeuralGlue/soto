c5c599e5882c63ef1c310f2d1c417193cfc5bbe3
Charles Young <charles.young@neuralglue.com>
2020-09-14 16:33:07 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -253,7 +253,7 @@ let package = Package(
         .library(name: "SotoXRay", targets: ["SotoXRay"])
     ],
     dependencies: [
-        .package(url: "https://github.com/NeuralGlue/soto-core.git", .upToNextMinor(from: "5.0.0-alpha.6"))
+        .package(url: "https://github.com/NeuralGlue/soto-core.git", .upToNextMinor(.branch("5.x.x"))
     ],
     targets: [
         .target(name: "SotoACM", dependencies: ["SotoCore"], path: "./Sources/Soto/Services/ACM"),
